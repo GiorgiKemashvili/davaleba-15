fbdf118a8d3d4b793f3b5e744c052752149930fc
GiorgiKemashvili <163575886+GiorgiKemashvili@users.noreply.github.com>
2024-05-02 16:04:45 +0400

    Added davaleba-3

--- script.js
+++ script.js
@@ -8,3 +8,24 @@ while (j <= 50) {
     console.log(j);
     j++;
 }
+// 3. davaleba-3
+let namesArray = ['Alice', 'Bob', 'Charlie', 'David', 'Eve'];
+console.log(namesArray);
+
+namesArray.pop();
+console.log(namesArray);
+
+namesArray.shift();
+console.log(namesArray);
+
+namesArray.unshift('Zoe');
+console.log(namesArray);
+
+namesArray.push('Frank');
+console.log(namesArray);
+
+delete namesArray[1];
+console.log(namesArray);
+
+namesArray[1] = 'Grace';
+console.log(namesArray);
